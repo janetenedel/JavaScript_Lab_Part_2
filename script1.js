20cee16011544af9b48b6c116e5450abc672c032
JaneteBirckNedel <janete_nedel@hotmail.com>
2019-02-20 20:08:28 -0500

    JavaScript Lab Part 2

--- script1.js
+++ script1.js
@@ -1,39 +1,67 @@
-let startGame = confirm("Do you want to start the game?");
-if (startGame == false) {
-  console.log("See you soon");
+function startGame() {
+  let startGame1 = "";
 
-} else {
-  let userName = prompt('What is your battle name?');
-  userPlayer(userName);
-  function userPlayer(userName) {
-  console.log(`Your username is ${userName}. Let's start the game!`);
-  console.log("");
-}
+  while (startGame1 === "") {
+    startGame1 = confirm("Do you want to start the game?");
+    if (startGame1 == false) {
+      console.log("See you soon");
+      return;
+    }
 
-let grantPoints = 10;
-let userPoints = 40;
-let user = userName;
-let wins = 0;
+    else {
+      function startCombat() {
+        let userName = prompt('What is your battle name?');
+        userPlayer(userName);
+        function userPlayer(userName) {
+          console.log(`Your battle name is ${userName}. Let's start the game!`);
+          console.log("");
+        }
+
+        function getDamage(min, max) {
+          return Math.floor(Math.random() * (max - min)) + min;
+        }
 
 
-while (wins < 3 && grantPoints >= 0) {
-    let getRandomGrant = Math.floor(Math.random() * 2) + 1;
-    let getRandomUser = Math.floor(Math.random() * 2) + 1;
-    grantPoints -= getRandomGrant;
-    userPoints -= getRandomUser;
-    console.log(`${user} has ${userPoints} health points left, Grant the almighty has ${grantPoints} health points left`);
-   
-    
-    if(grantPoints <= 0){
-      wins++;
-      console.log(`WIN #${wins} FOR ${user}!`);
-      grantPoints = 10;
-    } 
-    if (wins === 3) {
-      console.log(`${user} WINS!`);
-    } 
-    if (userPoints <= 0) {
-      console.log(`GRANT THE ALMIGHTY WINS!`);
-    } 
+        let grantPoints = 10;
+        let userPoints = 40;
+        let user = userName;
+        let wins = 0;
+        let attack = "";
+
+
+        while (wins < 3 && grantPoints >= 0) {
+          let attack = prompt(`Do you want to attack or quit?`);
+
+          if (attack === "quit" || attack == null) {
+            console.log(`${user} left the game`);
+            return;
+          }
+
+          if (attack == "attack") {
+            grantPoints -= getDamage(1, 5);
+            userPoints -= getDamage(1, 5);
+            console.log(`${user} has ${userPoints} health points left, Grant the almighty has ${grantPoints} health points left`);
+
+
+            if (grantPoints <= 0) {
+              wins++;
+              console.log(`WIN #${wins} FOR ${user}!`);
+              grantPoints = 10;
+
+              if (wins === 3) {
+                console.log(`${user} WINS!`);
+                // break;
+              }
+              if (userPoints <= 0) {
+                console.log(`GRANT THE ALMIGHTY WINS!`);
+              }
+
+            }
+          }
+        }
+      } startCombat();
+    }
   }
 }
+startGame();
+
